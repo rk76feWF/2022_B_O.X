03b742d8bf7dd0802156f2c319bb6f7f5a48c80f
FUKUMOTO Yuki <rk76fewf@gmail.com>
2022-09-12 15:32:20 +0900

    [Add] Lチカ

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <xc.h>
+#include "config.h"
+#define FCY 16000000UL
+#include <libpic30.h>
+
+#define LED1 _LATE3
+#define LED2 _LATE2
+#define LED3 _LATE1
+
+void setup(void);
+
+int main(void)
+{
+    setup();
+
+    while (1)
+    {
+        LED1 = ~LED1;
+        __delay_ms(1000);
+        LED2 = ~LED2;
+        __delay_ms(1000);
+        LED3 = ~LED3;
+        __delay_ms(1000);
+    }
+
+    return 0;
+}
+
+void setup(void)
+{
+    OSCCON = 0x0000;
+    _COSC = 0b001; // FRCPLL
+    _NOSC = 0b001; // FRCPLL
+    CLKDIV = 0x0000;
+
+    // Analog or Digital
+    ANSB = 0x0000;
+    ANSC = 0x0000;
+    ANSD = 0x0000;
+    ANSE = 0x0000;
+    ANSF = 0x0000;
+    ANSG = 0x0000;
+
+    // Input or Output
+    TRISB = 0xffff;
+    TRISC = 0xffff;
+    TRISD = 0xffff;
+    TRISE = 0xffff;
+    TRISF = 0xffff;
+    TRISG = 0xffff;
+
+    _TRISE3 = 0; // LED1
+    _TRISE2 = 0; // LED2
+    _TRISE1 = 0; // LED3
+
+    return;
+}
